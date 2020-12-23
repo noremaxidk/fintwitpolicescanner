9fddabe188d56a80921a51c09916d3ab7f960e8e
noremaxidk <76402774+noremaxidk@users.noreply.github.com>
2020-12-22 20:09:03 -0800

    Delete reader.py

--- project/reader.py
+++
@@ -1,6 +0,0 @@
-#idk why I made this more than one file. 
-
-import pandas as pd
-
-#make sure to specify the path to the sheet that contains the names. 
-fintwit_police_scanner = pd.read_csv(r'C:path\to\excelsheet.csv')
